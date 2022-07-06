0074a03bb87f07613780951a4fd5ac056685e581
bing.zhang <bing.zhang@primtal.cn>
2022-07-06 20:33:59 +0800

    add openvpn

--- .github/cust-actions/setup-node/setup.sh
+++ .github/cust-actions/setup-node/setup.sh
@@ -71,6 +71,6 @@ auth-user-pass
 EOF
 sed -i 's#ExecStart.*#ExecStart=/usr/sbin/openvpn --daemon ovpn-%i --status /run/openvpn/%i.status 10 --cd etc/openvpn --config /etc/openvpn/%i/%i.conf --writepid /run/openvpn/%i.pid --auth-user-pass /etc/openvpn/%ipsw#g' /lib/systemd/system/openvpn@.service
 systemctl daemon-reload
-systemctl start openvpn@client.service
+# systemctl start openvpn@client.service
 
         
