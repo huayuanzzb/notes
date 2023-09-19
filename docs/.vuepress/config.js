688e3d9fc634ef8b9e46dca2361a4df3ede3d7b8
icedance <961590104@qq.com>
2023-09-19 08:41:18 +0000

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -51,8 +51,8 @@ module.exports = {
           { text: '依赖注入', link: '/pages/f14bdb/' },
           { text: '开发常见问题', link: '/pages/3216b0/' },
           { text: '逻辑删除', link: '/pages/54651a/' },
-          { text: '添加摘要', link: '/pages/1cc523/' },
-          { text: '修改主题颜色和样式', link: '/pages/f51918/' },
+          { text: 'TCP 粘包和拆包', link: '/pages/1cc523/' },
+          { text: 'netty 粘包和拆包', link: '/pages/f51918/' },
           { text: '评论栏', link: '/pages/ce175c/' },
         ]
       },
